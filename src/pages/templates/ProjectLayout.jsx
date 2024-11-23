430fde61c49f2308cf2965a87d8022a65475203a
LukeZHar <15649@coderacademy.edu.au>
2024-11-23 11:46:38 +1000

    Fix: Netlify deploy for react routing fixed

--- src/pages/templates/ProjectLayout.jsx
+++ src/pages/templates/ProjectLayout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 
-export default function ProjectLayout(props){
-    return(
+export default function ProjectLayout(props) {
+    return (
         <div>
             <h1>Author</h1>
             <h3>Luke</h3>
 
             <Outlet />
-            
+
             <h6>Copyright {new Date().getFullYear()}</h6>
         </div>
     )
